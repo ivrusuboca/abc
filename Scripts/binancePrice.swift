5f725790debce4ccde5872b52a6353fd7266135f
ionut v. rusu boca <ivrusuboca@gmail.com>
2020-07-12 22:48:14 +0800

    #2: set AF request a timeout and a cURL debug-like output; main replaces sleep with DispatchGroup when waiting for AF response

--- Scripts/binancePrice.swift
+++ Scripts/binancePrice.swift
@@ -16,17 +16,25 @@ guard CommandLine.arguments.count > 1 else
 let pair = CommandLine.arguments[1]
 print("getting price for pair : \(pair) ... ")
 
+let group = DispatchGroup()
+group.enter()
+
 AF
    .request("https://api.binance.com/api/v3/ticker/price?symbol=\(pair)")
+   { req in req.timeoutInterval = 2 }                        // timeout set to 2 seconds
+   .cURLDescription { desc in print("curl: \(desc) ") }      // for debugging purposes
    .responseJSON(queue: DispatchQueue.global(qos: .utility)) // not in .main queue
 { res in
+   
    let json = try! JSON(data: res.data!)
    let price = Decimal(string: json["price"].string!)!
    let symbol = json["symbol"].string!
    print("\(symbol) : \(price) ")
+   
+   group.leave()
 }
 
-sleep(3) // sleep few seconds
+group.wait()
 
 print("done.")
 
