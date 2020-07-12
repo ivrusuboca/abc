30d0ba966ba2a4c3c1d2a2ccfb19b953eb6877e0
ionut v. rusu boca <ivrusuboca@gmail.com>
2020-07-12 22:30:18 +0800

    #2: quick, dirty and error-prone way to get the pair's price

--- Scripts/binancePrice.swift
+++ Scripts/binancePrice.swift
@@ -2,6 +2,9 @@
 
 import Foundation
 
+import Alamofire // @Alamofire ~> 5.2
+import SwiftyJSON // @SwiftyJSON ~> 5.0.0
+
 guard CommandLine.arguments.count > 1 else
 {
    let scriptName = CommandLine.arguments[0].components(separatedBy: "/").last!
@@ -12,4 +15,18 @@ guard CommandLine.arguments.count > 1 else
 
 let pair = CommandLine.arguments[1]
 print("getting price for pair : \(pair) ... ")
+
+AF
+   .request("https://api.binance.com/api/v3/ticker/price?symbol=\(pair)")
+   .responseJSON(queue: DispatchQueue.global(qos: .utility)) // not in .main queue
+{ res in
+   let json = try! JSON(data: res.data!)
+   let price = Decimal(string: json["price"].string!)!
+   let symbol = json["symbol"].string!
+   print("\(symbol) : \(price) ")
+}
+
+sleep(3) // sleep few seconds
+
+print("done.")
 
