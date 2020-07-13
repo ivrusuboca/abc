e99a719b1a7b89778722858a81715eb2c7854e5e
ionut v. rusu boca <ivrusuboca@gmail.com>
2020-07-13 13:54:29 +0800

    #2: captured price logic inside a function which returns a ( pair, price) tuple if successful or throws an exception

--- Scripts/binancePrice.swift
+++ Scripts/binancePrice.swift
@@ -16,50 +16,83 @@ guard CommandLine.arguments.count > 1 else
 let pair = CommandLine.arguments[1]
 print("getting price for pair : \(pair) ... ")
 
-let group = DispatchGroup()
-group.enter()
-
-AF
-   .request("https://api.binance.com/api/v3/ticker/price?symbol=\(pair)")
-   { req in req.timeoutInterval = 2 }                        // timeout set to 2 seconds
-   .cURLDescription { desc in print("curl: \(desc) ") }      // for debugging purposes
-   .responseJSON(queue: DispatchQueue.global(qos: .utility)) // not in .main queue
-{ res in
-   
-   defer
-   {
-      group.leave() // whatever happens next, this will be called
-   }
-   
-   switch res.result
-   {
-   case .success:
-      guard let data = res.data, let json = try? JSON(data: data) else
-      {
-         print("failure : invalid JSON format for response : \(res.value) ")
-         return
-      }
-      guard
-         let symbol = json["symbol"].string,
-         let p = json["price"].string,
-         let price = Decimal(string: p) else
-      {
-         if let msg = json["msg"].string, let code = json["code"].int
-         {
-            print("failure : code \(code) w/ message : \(msg) ")
-         }else
-         {
-            print("failure : \(json) ")
-         }
-         return
-      }
-      print("\(symbol) : \(p) ")
-   case .failure(let err):
-      print("failure : \(res.value) w/ error : \(err) ")
-   }
+enum Failure : Error
+{
+   case withMessage(msg: String)
+   case withCode(code:Int, msg: String)
 }
 
-group.wait()
+func getPrice(ofPair pair:String) throws -> (String, Decimal)
+{
+
+   var result:(String, Decimal)?
+   var failure:Failure?
+   
+   let group = DispatchGroup()
+   group.enter()
+
+   AF
+      .request("https://api.binance.com/api/v3/ticker/price?symbol=\(pair)")
+      { req in req.timeoutInterval = 2 }                        // timeout set to 2 seconds
+      .cURLDescription { desc in print("curl: \(desc) ") }      // for debugging purposes
+      .responseJSON(queue: DispatchQueue.global(qos: .utility)) // not in .main queue
+   { res in
+      
+      defer
+      {
+         group.leave() // whatever happens next, this will be called
+      }
+      
+      switch res.result
+      {
+      case .success:
+         guard let data = res.data, let json = try? JSON(data: data) else
+         {
+            failure = Failure.withMessage(msg: "invalid JSON format for response : \(res.value)")
+            return
+         }
+         guard
+            let symbol = json["symbol"].string,
+            let p = json["price"].string,
+            let price = Decimal(string: p) else
+         {
+            if let msg = json["msg"].string, let code = json["code"].int
+            {
+               failure = Failure.withCode(code: code, msg: msg)
+            }else
+            {
+               failure = Failure.withMessage(msg: "\(json)")
+            }
+            return
+         }
+         result = (symbol, price)
+      case .failure(let err):
+         failure = Failure.withMessage(msg: "\(err)")
+      }
+   }
+   group.wait()
+
+   assert(result != nil || failure != nil)
+   
+   if let r = result
+   {
+      return r
+   }
+   throw failure!
+   
+} // func
+
+do
+{
+   let (p, price) = try getPrice(ofPair: pair)
+   print("\(p) : \(price) ")
+}catch Failure.withMessage(let msg) {
+   print("failure with message : \(msg)")
+}catch Failure.withCode(let code, let msg) {
+   print("failure with code : \(code) and message : \(msg)")
+}catch {
+   print("unexpected failure : \(error) ")
+}
 
 print("done.")
 
